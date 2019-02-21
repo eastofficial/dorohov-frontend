e75cf7ad1d1ef9635776861003bcb38da7435a19
eastsofficial <androsov.dima2013@yandex.ru>
2019-02-21 12:08:47 +0300

    Сырая вёрстка Landing

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$( document ).ready(function() {
+    $(".menu-mob").click(function(){
+        $(".menu-mobile").css({'display':'block'});
+    });
+    $(".cancel-mob-menu").click(function(){
+        $(".menu-mobile").css({'display':'none'});
+    });
+});
+
+
+
+
+    
